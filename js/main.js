5b542cb04dfd01ad2bc370d3754ea421c9230b15
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-09-21 23:52:01 +0000

    js:name variables

--- js/main.js
+++ js/main.js
@@ -1,23 +1,56 @@
+let nextButton = document.getElementById('next');
+let refreshBtn = document.getElementById('reset');
+let headerText = document.getElementById('textHeader');
+let example = document.getElementById('exampleText');
+let explainer = documment.getElementById('explainText');
+
+let pageCounter = 0;
+let viewPages = [
+    {
+        "headerText": "filler1",
+        "nextButton": "filler2"
+    },
+
+    {
+        "headerText": "fller3",
+        "nextButton": "filler4",
+        "example": "filler5"
+    },
+
+    {
+        "headerText": "filler6",
+        "nextButton": "filler7",
+        "example": "filler8",
+        "explainer": "filler9",
+
+    },
+
+    {
+        "headerText": "filler10",
+        "nextButton": "filler11",
+        "example": "filler12",
+        "explainer": "filler12",
+    },
+
+    {
+        "headerText": "symbols",
+        "nextButton": "filler13",
+        "example": "filler14",
+        "explainer": "filler15",
+    },
+
+    {
+        "headerText": "1symbol",
+        "example": "filler16",
+        "explainer": "1symbol",
+    }
+];
 
 
-// function firstUpdate(){
-let btnGo = document.getElementById("next");
-
-btnGo.addEventListener('click', () =>{
-document.getElementById("maintext").innerHTML = "THINK OF A NUMBER";
-document.getElementById("next").innerHTML = "NEXT";
-
-// firstUpdate();
-});
 
 
 
 
-   
-   
-   
-   
-  
 
 
 
