ae2a7534cfece99d65768b5880640d100e360885
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-09-21 20:28:04 +0000

    bootstrap/test eventlistener initial page

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+
+// function firstUpdate(){
+let btnGo = document.getElementById("next");
+
+btnGo.addEventListener('click', () =>{
+document.getElementById("maintext").innerHTML = "THINK OF A NUMBER";
+document.getElementById("next").innerHTML = "NEXT";
+
+// firstUpdate();
+});
+
+
+
+
+   
+   
+   
+   
+  
+
+
+
