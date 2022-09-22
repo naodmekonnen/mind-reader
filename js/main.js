cb45e14ab3b59125566df3b55a8ee0c037a9a341
naodmekonnen <naod.mekonnen@student.awesomeinc.org>
2022-09-22 16:40:10 +0000

    e target and event listener

--- js/main.js
+++ js/main.js
@@ -1,50 +1,82 @@
-let nextButton = document.getElementById('next');
-let refreshBtn = document.getElementById('reset');
 let headerText = document.getElementById('textHeader');
+let nextButton = document.getElementById('next');
 let example = document.getElementById('exampleText');
 let explainer = documment.getElementById('explainText');
+let resetBtn = document.getElementById('reset');
 
-let pageCounter = 0;
-let viewPages = [
+
+let pageView = [
     {
-        "headerText": "filler1",
-        "nextButton": "filler2"
+        "headerText": "I can read your mind",
+        "nextButton": "start"
     },
 
     {
-        "headerText": "fller3",
-        "nextButton": "filler4",
-        "example": "filler5"
+        "headerText": "Pick a number from 0 - 99",
+        "nextButton": "NEXT",
+        "example": "When you have thar number click next",
+        "resetBtn": "Try Again"
     },
 
     {
-        "headerText": "filler6",
-        "nextButton": "filler7",
-        "example": "filler8",
-        "explainer": "filler9",
+        "headerText": "Add both digits together to get a new number",
+        "nextButton": "NEXT",
+        "example": "Ex: 14 is 1 + 4 = 5",
+        "explainer": "click next to proceed",
+        "resetBtn": "Try again"
 
     },
 
     {
-        "headerText": "filler10",
-        "nextButton": "filler11",
-        "example": "filler12",
-        "explainer": "filler12",
+        "headerText": "Subtract your new number from the original number",
+        "nextButton": "NEXT",
+        "example": "EX: 14 - 5 = 9",
+        "explainer": "click next to proceed",
+        "resetBtn": "Try again"
+
     },
 
     {
         "headerText": "symbols",
-        "nextButton": "filler13",
-        "example": "filler14",
-        "explainer": "filler15",
+        "nextButton": "REVEAL",
+        "example": "Find your new number",
+        "explainer": "Note the symbol beside the number",
+        "resetBtn": "Try again"
+
     },
 
     {
         "headerText": "1symbol",
         "example": "filler16",
         "explainer": "1symbol",
+        "resetBtn" : "Try again"
     }
 ];
+
+
+let pageCount = 0;
+
+textHeader.textContent=pageView[pageCount].textHeader;
+next.textContent=pageView[pageCount].next;
+exampleText.textContent=pageView[pageCount].exampleText;
+explainText.textContent=pageView[pageCount].explainText;
+resetBtn.textContent=pageView[pageCount].resetBtn;
+
+nextButton.addEventListener('click', newPage);
+function newPage(e);
+
+ if(e.target.id="next"){
+    pageCount++;
+    console.log(pageCount);
+ };
+
+resetBtn.addEventListener('click', resetPage);
+ function resetPage(e);
+ if(e.target.id = "reset"){
+    pageCount = 0;
+ }
+
+
 
 
 
